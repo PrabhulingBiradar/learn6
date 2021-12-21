8fa6067cb527b66b1d4e4c8283e2c7b4d57ff27c
PrabhulingBiradar <prabulingabiradar537.xworkz@gmail.com>
2021-12-21 10:57:44 +0530

    learning floatjava

---
+++ floatjava.java
@@ -0,0 +1,21 @@
+class floatjava
+{
+public static void main(String[]args){
+
+
+float[] flo={22.005f,52.65466f,55.4646f,33.555f,89.46000f,91.646464f,26.0000f,25.646400f,97.5454f,54.454500f};
+   System.out.println(flo[0]);
+   System.out.println(flo[1]);
+ System.out.println(flo[2]);
+ System.out.println(flo[3]);
+ System.out.println(flo[4]);
+ System.out.println(flo[5]);
+ System.out.println(flo[6]);
+ System.out.println(flo[7]);
+ System.out.println(flo[8]);
+ System.out.println(flo[9]);
+
+
+
+}
+}
